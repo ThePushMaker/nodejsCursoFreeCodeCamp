e0922687baf43f8a53302aac46353b6cff29e505
Martín Samuel Calderón Ojeda <martincalderon@iMac-de-Martin.local>
2023-02-28 16:18:59 -0700

    os

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const os = require('os'); 
 
-console.log(os.type());
+//obtener sistema operativo
+// console.log(os.type());
+
+//obtener directorio principal
+// console.log(os.homedir());
+
+//saber cuanto tiempo ha estado trasncurrido desde que se inicio el sistema operativo en segundos
+console.log(os.uptime());
