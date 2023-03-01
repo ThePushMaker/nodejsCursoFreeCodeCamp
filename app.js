e20ade050828e1b831449fd65159fb68687f40ae
Martín Samuel Calderón Ojeda <martincalderon@iMac-de-Martin.local>
2023-03-01 16:50:34 -0700

    setimmediate

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const os = require('os'); 
+//el modulo timers contiene funciones que ejecutan codigo luego de un cierto periodo de tiempo
+//1 segundo es equivalente a 1000 milisegundos
 
-//obtener sistema operativo
-// console.log(os.type());
+//setTimeout(funcion, retraso, argumento1, arguemento2);
 
-//obtener directorio principal
-// console.log(os.homedir());
+function mostrarTema(tema){
+    console.log(`Estoy aprendiendo ${tema}`);
+}
 
-//saber cuanto tiempo ha estado trasncurrido desde que se inicio el sistema operativo en segundos
-console.log(os.uptime());
+function sumar(a, b){
+    console.log(a+b);
+}
+
+//retrasa la ejecución 1 segundo
+// setTimeout(mostrarTema, 1000, 'Node.js');
+
+// setTimeout(sumar, 2000, 5, 6);
+
+
+//setImmediate()
+//para ejecutar codigo asincrono en la proxima iteracion del ciclo de eventos (lo más pronto posible)
+
+//setImmediate(function, argumento1, argumento2);
+
+//la función se va a ejecutar después del codigo sincrono 
+
+// console.log('Antes de setImmediate()');
+
+// setImmediate(mostrarTema, 'Node.js');
+
+// console.log('Después de setImmediate()');
---
+++ modulo_os/app.js
@@ -0,0 +1,13 @@
+const os = require('os'); 
+
+//obtener sistema operativo
+// console.log(os.type());
+
+//obtener directorio principal
+// console.log(os.homedir());
+
+//saber cuanto tiempo ha estado trasncurrido desde que se inicio el sistema operativo en segundos
+// console.log(os.uptime());
+
+//obtener información de usuario
+console.log(os.userInfo());
